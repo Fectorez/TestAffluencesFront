48772e3835da711f2945d3f09dbcc2a534ac55f3
Jérémie <jeremieroland@orange.fr>
2021-02-15 21:32:30 +0100

    component check

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CheckComponent } from './check/check.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CheckComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CheckComponent } from './check/check.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckComponent
   ],
   imports: [
     BrowserModule,
